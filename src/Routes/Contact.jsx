7c681f533eaf79ac2241c749d6bdc2553f0ef08c
Manuel Nuñez <mnrodriguez001@gmail.com>
2023-04-07 01:49:02 -0300

    Examen Terminado

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Form from '../Components/Form'
-
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
+import { useTheme } from '../Components/utils/global.context'
 
 const Contact = () => {
+  const {theme} = useTheme()
   return (
-    <div>
-      <h2>Want to know more?</h2>
-      <p>Send us your questions and we will contact you</p>
+    <div className={theme.footerBody}>
+      <h2 className={theme.title}>Want to know more?</h2>
+      <p className={theme.text}>Send us your questions and we will contact you</p>
       <Form/>
     </div>
   )
